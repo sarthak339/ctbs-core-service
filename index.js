d374d169594387b83479abaa92f81ad010713cd7
sarthak <sarthakofficial920@gmail.com>
2025-04-06 16:52:35 +0530

    remove some files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config(); 
+
 global.__basedir = __dirname;
  
 async function init(){
   try{
     await require('./routes').init();
+    // await require('./services/techBlogs').fetchAllTechBlogs(); 
+
   }catch(error){
     console.error(`failed to start the server , error: ${error}`);  
   }
